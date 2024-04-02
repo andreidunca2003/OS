423e7930096a6705491122923085c40a090c2071
andreidunca2003 <149617084+andreidunca2003@users.noreply.github.com>
2024-04-02 09:09:52 +0300

    Update

--- myprog.c
+++ myprog.c
@@ -8,10 +8,26 @@
 #define MAX 257
 
 struct FileMetadata {
-    char name[256]; 
+    char name[256];
     unsigned int permissions;
     unsigned long size;
 };
+
+struct FileMetadata captureMetadata(const char *path) {
+    struct FileMetadata metadata;
+    struct stat st;
+
+    if (stat(path, &st) == 0) {
+        strncpy(metadata.name, path, sizeof(metadata.name));
+        metadata.permissions = st.st_mode & (S_IRWXU | S_IRWXG | S_IRWXO);
+        metadata.size = st.st_size;
+    } else {
+        perror("stat");
+        exit(EXIT_FAILURE);
+    }
+
+    return metadata;
+}
 
 void captureDirectory(const char *basePath) {
     char path[MAX];
@@ -23,17 +39,27 @@ void captureDirectory(const char *basePath) {
         exit(EXIT_FAILURE);
     }
 
+    while ((dp = readdir(dir)) != NULL) {
+        if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0) {
+            sprintf(path, "%s/%s", basePath, dp->d_name);
+            struct FileMetadata metadata = captureMetadata(path);
+            printf("%s\n", metadata.name);
+            if (S_ISDIR(metadata.permissions)) {
+                captureDirectory(path);
+            }
+        }
+    }
 
     closedir(dir);
 }
 
 int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <directory_path>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
 
-    printf("Before:\n");
+    printf("Before monitoring:\n");
     captureDirectory(argv[1]);
 
-    printf("\nAfter:\n");
-    captureDirectory(argv[1]);
-
-    return 0;
-}
+   
