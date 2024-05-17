0ce8db64dd68d8a0b8a0f610e09a3d5b9216c510
andreidunca2003 <149617084+andreidunca2003@users.noreply.github.com>
2024-05-17 13:38:54 +0300

    Update myprog.c

--- myprog.c
+++ myprog.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <dirent.h>
 #include <fcntl.h>
 #include <time.h>
-#include <stdbool.h>
-#include <sys/wait.h>
-
 #define MAX_PATH_LENGTH 4096
 #define MAX_FILES 100
 
